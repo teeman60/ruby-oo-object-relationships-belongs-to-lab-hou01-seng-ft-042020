450d05f79477f9fb4ee2ba39e4dc73bae53143a7
Taiye Salami <taiye41@gmail.com>
2020-04-23 10:49:28 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Post
+
+    attr_accessor :author
+    attr_accessor :title
+    attr_accessor :name
+
+    def initialize
+        @title 
+        @author
+        @name 
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+
+    attr_accessor :artist
+    attr_accessor :title
+
+    def initialize
+        @title 
+        @artist
+    end
+
+end
